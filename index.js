9fcc7225c4d545a894b0298cacad39830f1d1fa0
Alexander Pinnecke <alexander.pinnecke@googlemail.com>
2016-02-17 16:47:41 +0100

    Added winner page

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var express = require('express'),
     browserify = require('browserify-middleware'),
     less = require('less-middleware'),
+    slashes = require('connect-slashes'),
     app = express();
 
 app.use('/app.js', browserify(__dirname + '/lib/app.js'));
 app.use('/hammer.js', express['static'](__dirname + '/node_modules/hammerjs/hammer.js'));
 app.use('/angular.hammer.js', express['static'](__dirname + '/node_modules/angular-hammer/angular.hammer.js'));
 app.use(less(__dirname + '/lib'));
-app.use('/', express['static'](__dirname + '/lib'));
-app.all('/*', function(req, res) {
+
+app.use(slashes(false));
+
+app.all('/:var(wheel|participants|winners)?', function(req, res) {
     res.sendFile(__dirname + '/lib/index.html');
 });
+app.use('/', express['static'](__dirname + '/lib'));
 
 app.listen('3001', function () {
     console.log('Listening on http://0.0.0.0:3001');
